dfb1eb4a3d61c5a9d9f614d1d4c5f188d8e3f4f4
David <bischof.david.db@gmail.com>
2023-10-08 22:46:30 +0200

    Put Reveal infont of everything

--- Client/wishchat/src/Components/Wrapper.jsx
+++ Client/wishchat/src/Components/Wrapper.jsx
@@ -3,12 +3,14 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { SimpleAccount } from './SimpleAccount';
 import { WholeChat } from './Wholechat';
+import { Welcome } from './Welcome';
 export function Wrapper() {
   
   const [me, setMe] = useState("")
 
   return (
     <Router>
+      <Welcome /> 
       <Routes>
         <Route path="/" element={<SimpleAccount setMe={setMe}/>} />
         <Route path="/Chat" element={<WholeChat me={me}/>} />
