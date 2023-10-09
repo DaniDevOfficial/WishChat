c76fb1e05a5c157807611908e758b2acb027144b
David <bischof.david.db@gmail.com>
2023-10-09 22:36:30 +0200

    added better styling for Welcome page

--- Client/wishchat/src/Components/WelcomeSite.jsx
+++ Client/wishchat/src/Components/WelcomeSite.jsx
@@ -1,11 +1,25 @@
 import React from "react"
 import '../Styles/Welcome.css'
+import  Layout from "../Images/Layout idea.jpg"
 export function WelcomeSite() {
 
     return (
         <div className="WelcomeContainer">
                 <div className="WelcomeWrapper">
-                    wasd
+                    
+                    <div className="WelcomeTextContainer">
+                        <div className="MainWelcome">
+                        Welcome to WishChat
+                        </div>
+                        <div className="SecondaryWelcome">
+                            This is a small Chatapp created with react and currently with a firebase backend but enjoy your time on it.
+                        </div>
+                    </div>
+
+                    <div className="WelcomeImageContainer">
+                        <img src={Layout} alt="" />
+                    </div>
+
                 </div>
         </div>
     )
