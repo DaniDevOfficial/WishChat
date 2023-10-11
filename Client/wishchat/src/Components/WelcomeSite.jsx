9ae9147383ed315530af0f8d5d4e7d5733ca5b76
David21092 <bischof.david.db@gmail.com>
2023-10-11 13:47:08 +0200

    added Login screen in welcome page

--- Client/wishchat/src/Components/WelcomeSite.jsx
+++ Client/wishchat/src/Components/WelcomeSite.jsx
@@ -1,9 +1,20 @@
-import React from "react"
+import React, { useState } from 'react';
 import '../Styles/Welcome.css'
 import Layout from "../Images/Layout idea.jpg"
 import { WelcomeAnimation } from "./WelcomeAnimation"
-export function WelcomeSite() {
+import { useNavigate } from 'react-router-dom';
 
+export function WelcomeSite({ setMe }) {
+    const [name, setName] = useState('');
+    let navigate = useNavigate();
+
+    const handleNameChange = (event) => {
+        setName(event.target.value);
+    };
+    const handleSubmit = () => {
+        navigate(`/Chat`);
+        setMe(name)
+    }
     return (
         <div className="WelcomeContainer">
             <WelcomeAnimation />
@@ -16,6 +27,35 @@ export function WelcomeSite() {
                     </div>
                     <div className="SecondaryWelcome">
                         This is a small Chatapp created with react and currently with a firebase backend but enjoy your time on it.
+                    </div>
+                    <div className="SignUpContainer">
+                        <form onSubmit={handleSubmit}>
+                            <label htmlFor="name" class="InputLabel">Name</label>
+                            <input
+                                className='AccountInput'
+                                type="text"
+                                id="name"
+                                name="name"
+                                value={name}
+                                onChange={handleNameChange}
+                                required={true}
+                                placeholder='Username'
+                            />
+                            <label htmlFor="password" class="InputLabel">Password</label>
+
+                            <input
+                                className='AccountInput'
+                                type="password"
+                                id="password"
+                                name="password"
+                                required={true}
+                                placeholder='Password'
+                            />
+                            <div className="buttonCenter">
+                                <input class="SubmitButton" type="submit" />
+                            </div>
+
+                        </form>
                     </div>
                 </div>
 
