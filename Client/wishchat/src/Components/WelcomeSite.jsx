a0e8405967ea981b14964b5401dd8cc012e77c3f
David21092 <bischof.david.db@gmail.com>
2023-10-12 14:37:05 +0200

    added a real welcome grafic and a fav.icon but this needs to be better

--- Client/wishchat/src/Components/WelcomeSite.jsx
+++ Client/wishchat/src/Components/WelcomeSite.jsx
@@ -52,7 +52,7 @@ export function WelcomeSite({ setMe }) {
                                 placeholder='Password'
                             />
                             <div className="disclaimer">
-                                (the account data isnt actualy stored)
+                                (the account data isn't actualy saved so dont worry about remembering your Credentials)
                             </div>
                             <div className="buttonCenter">
                                 <input
@@ -69,7 +69,16 @@ export function WelcomeSite({ setMe }) {
                 </div>
 
                 <div className="WelcomeImageContainer">
-                    <img src={Layout} alt="" />
+                    <div className="WelcomeImageWelcome">
+                        Welcome
+                    </div>
+                    <div className="WelcomeImage">
+                        <img src="https://firebasestorage.googleapis.com/v0/b/wishchatprog2.appspot.com/o/Images%2FCapybara.png?alt=media&token=4facbe5d-1d95-49ae-b395-aa989fe74ce3&_gl=1*5nfsu0*_ga*MjAzNzYyNzU5LjE2ODM4Mjk0MDU.*_ga_CW55HF8NVT*MTY5NzExMjM2My41MS4xLjE2OTcxMTIzODMuNDAuMC4w" alt="" />
+                    </div>
+
+                    <div className='WelcomeImageHaveFun'>
+                        Have Fun Chatting
+                    </div>
                 </div>
 
             </div>
