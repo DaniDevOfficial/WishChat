8d11ba1fdcdea6ad06a7e2efb29a5ad9910173d1
David21092 <bischof.david.db@gmail.com>
2023-10-12 22:31:34 +0200

    added a funny and idk what else

--- Client/wishchat/src/Components/WelcomeSite.jsx
+++ Client/wishchat/src/Components/WelcomeSite.jsx
@@ -3,7 +3,14 @@ import '../Styles/Welcome.css'
 import Layout from "../Images/Layout idea.jpg"
 import { WelcomeAnimation } from "./WelcomeAnimation"
 import { useNavigate, Link } from 'react-router-dom';
-
+import Napoleon from "../Songs/ThersNotingWeCanDo.mp3"
+import CatSmurf from "../Songs/LLL.mp3"
+import YoungFly from "../Songs/YoungFly.mp3"
+import Capy from "../Songs/Cap.mp3"
+import Delfin from "../Songs/Delvin.mp3"
+import SuSuS from "../Songs/asus.mp3"
+import Skelly from "../Songs/Skelly.mp3"
+import Airliner from "../Songs/Airliner.mp3"
 export function WelcomeSite({ setMe }) {
     const [name, setName] = useState('');
     let navigate = useNavigate();
@@ -15,12 +22,33 @@ export function WelcomeSite({ setMe }) {
         navigate(`/Chat`);
         setMe(name)
     }
+    const soundUrls = [
+        Napoleon,
+        CatSmurf,
+        YoungFly,
+        Capy,
+        Delfin,
+        SuSuS,
+        Skelly,
+        Airliner,
+    ]
+    let counter = 0
+    function funny() {
+        counter++
+        if (counter == 20) {
+            const randomIndex = Math.floor(Math.random() * soundUrls.length);
+            const audio = new Audio(soundUrls[randomIndex]);
+            audio.play(); counter = 0
+        }
+    }
+
+
+
     return (
         <div className="WelcomeContainer">
             <WelcomeAnimation />
 
             <div className="WelcomeWrapper">
-
                 <div className="WelcomeTextContainer">
                     <div className="MainWelcome">
                         Welcome to WishChat
@@ -72,8 +100,8 @@ export function WelcomeSite({ setMe }) {
                     <div className="WelcomeImageWelcome">
                         Welcome
                     </div>
-                    <div className="WelcomeImage">
-                        <img src="https://firebasestorage.googleapis.com/v0/b/wishchatprog2.appspot.com/o/Images%2FCapybara.png?alt=media&token=4facbe5d-1d95-49ae-b395-aa989fe74ce3&_gl=1*5nfsu0*_ga*MjAzNzYyNzU5LjE2ODM4Mjk0MDU.*_ga_CW55HF8NVT*MTY5NzExMjM2My41MS4xLjE2OTcxMTIzODMuNDAuMC4w" alt="" />
+                    <div className="WelcomeImage" >
+                        <img src="https://firebasestorage.googleapis.com/v0/b/wishchatprog2.appspot.com/o/Images%2FCapybara.png?alt=media&token=4facbe5d-1d95-49ae-b395-aa989fe74ce3&_gl=1*5nfsu0*_ga*MjAzNzYyNzU5LjE2ODM4Mjk0MDU.*_ga_CW55HF8NVT*MTY5NzExMjM2My41MS4xLjE2OTcxMTIzODMuNDAuMC4w" onClick={funny} alt="" />
                     </div>
 
                     <div className='WelcomeImageHaveFun'>
